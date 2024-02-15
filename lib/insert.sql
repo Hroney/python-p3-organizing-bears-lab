b8ca3aa1a73a77f173f3432a4d24738d702cea4e
Hayden Roney <roneyhayden@gmail.com>
2024-02-15 12:16:36 -0600

    Finishes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'F', 'Black', 'Sleepy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'M', 'Brown', 'Angry', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'Black', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Polar', 'cold', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'Grey', 'Tired', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 16, 'F', 'Brown', 'Upset', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'F', 'Brown', 'Happy', 0);
